30b30eb5b055b9c2ead4f4409c8f61d9e44cad8e
Jay Johnston <jay@toptal.com>
2014-08-30 07:10:55 -0400

    removed page panel again

--- wp-content/themes/nyyimby_new_5th/main_tab.php
+++ wp-content/themes/nyyimby_new_5th/main_tab.php
@@ -2,38 +2,30 @@
 <div class="scroll_wrapper">
     <div class="padding content sidebar_nav_inner_news" id="sidebar_nav_inner">
         <div class="test_inner_news">
-            <ul class="media-list left_post_nav">
 
             <?php
                 global $post;
 
-                $current_post = $post; // remember the current post
-
                 $post = get_next_post(); // this uses $post->ID
-                if (empty($post)) {
-                    $post = $current_post;
-                } else {
-
-                    setup_postdata($post);
-                    $curDate = the_date('l F jS, Y', '', '', false);
-
-                    $post = get_previous_post();;
-                }
-
                 setup_postdata($post);
+                $curDate = the_date('l F jS, Y', '', '', false);
+                $post = get_previous_post(); // this uses $post->ID
+                setup_postdata($post);
+
+                $current_post = $post; // remember the current post
                 $current_date = the_date('l F jS, Y', '', '', false);
 
-                $noDate = 0;
-                $paged = (integer) $paged;
-                if (strcmp($curDate,$current_date)===false && $paged>1) {
+                if (strcmp($curDate,$current_date)===false) {
                     $noDate = 1;
                 }
+                //$noDate = pagination_noDate();
             ?>
 
+            <ul class="media-list left_post_nav">
               <?php 
                 if (empty($noDate)) : ?>
                 <li class="dateHeading">
-                    <?php echo $current_date; ?>
+                    <?php echo $current_date;?>
                 </li>
               <?php endif; ?> 
 
@@ -51,7 +43,9 @@
                         </a>
                     </div> <!-- // div.media-body -->
                 </li>
+            </ul>
 
+            <ul class="media-list left_post_nav">
                 <?php
                     for($i = 1; $i <= 19; $i++) {
                     $post = get_previous_post(); // this uses $post->ID
@@ -65,7 +59,7 @@
                     <li class="dateHeading">
                         <? echo $curDate; ?>
                     </li>
-                <?php endif; } ?> 
+                <? endif; } ?>
 
                 <li class="media">
                     <a class="pull-left" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="left_nav_link">
@@ -88,16 +82,16 @@
             </ul>
 
             <!-- Pagination -->
-            <ul class="pager">
+            <ul class="pager" style="display:block;visibility:hidden;position:absolute;">
                 <li class="next">
-                    <a href="<?php echo home_url(); ?>/page/<?php echo (($paged-1>0) ? ($paged+1) : 2); ?>"  class="next_link">
+                    <a href="<?php echo home_url(); ?>/page/<?php echo $paged+2;?>"  class="next_link">
                         <span class="meta-nav">&larr;</span> Older posts
                     </a>
                 </li>
 
                 <?php if ($paged > 0) :?>
                     <li class="previous">
-                        <a href="<?php echo home_url(); ?>/page/<?php echo (($paged-1>0) ? ($paged-1) : 1); ?>">
+                        <a href="<?php echo home_url(); ?>/page/<?php echo $paged-1;?>">
                             Newer posts
                             <span class="meta-nav">&rarr;</span>
                         </a>
