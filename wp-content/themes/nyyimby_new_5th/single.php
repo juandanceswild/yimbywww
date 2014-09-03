bc63db0a2fccc918b4842dd09e40ca6eb411f1bf
Jay Johnston <jay@toptal.com>
2014-09-03 17:44:22 -0400

    continued styling of new responsive layout

--- wp-content/themes/nyyimby_new_5th/single.php
+++ wp-content/themes/nyyimby_new_5th/single.php
@@ -17,20 +17,41 @@ get_header(); ?>
 $paged = (get_query_var('page')) ? get_query_var('page') : 1;
 ?>
 
-       <div class="row-fluid">
-            <div class="span3">
+       <div class="row-fluid white pt-top">
+            <div class="my-col span3">
                 <?php get_sidebar('leftTABS');?>
             </div>
 
-            <div class="span6">
+            <div class="my-col span6">
                 <div class="row-fluid">
                     <?php get_template_part('content', 'post'); ?>
                 </div>
             </div>
 
-            <div class="span3">
+            <div class="my-col span3">
                 ads go here
+                <div style="height:3000px; width:200px;"></div>
             </div>
+            <br class="clr">
         </div>
+
+<style>
+.my-col {
+  height:100%;
+  overflow-x:hidden;
+  overflow-y:scroll;
+}
+.clr {
+  clear:both;
+}
+.pt-top {
+  padding-top:4%;
+  padding-bottom:2%;
+  height:94%;
+}
+.white {
+  background-color:#fff;
+}
+</style>
 
 <?php get_footer();?>
