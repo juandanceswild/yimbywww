19855f4ef0b2293bd194e9544d16900e8f5bf3ca
Jay Johnston <jay@toptal.com>
2014-09-03 23:47:44 -0400

    adjusted the positioning of the fixed social icons

--- wp-content/themes/nyyimby_new_5th/js/FixedSocial.js
+++ wp-content/themes/nyyimby_new_5th/js/FixedSocial.js
@@ -82,6 +82,7 @@ var FixedSocial = FixedSocial || (function()
         {
 
             var desktopOffsetLeft = jQuery('div#cx').offset().left + jQuery('div#cx').width() - 22;
+            desktopOffsetLeft-=30;
             this.dom.icons.attr('style', 'position: fixed; top: 78px; left: ' + desktopOffsetLeft + 'px !important');
 
             // console.log( 'Desktop, add fixded right positioning.' );
