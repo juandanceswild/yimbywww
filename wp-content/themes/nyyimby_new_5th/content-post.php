ac7202913f5126914eec0dae72e7625f695f5396
Jay Johnston <jay@webjuju.com>
2014-09-12 10:07:39 -0700

    removed extra horizontal line after first post

--- wp-content/themes/nyyimby_new_5th/content-post.php
+++ wp-content/themes/nyyimby_new_5th/content-post.php
@@ -3,7 +3,7 @@
             <div id="stuff" class="jj">
                     <?php while ($wp_query->have_posts()) : $wp_query->the_post();  ?>
                         <div id="cx">
-                            <div class="post bordered">
+                            <div class="post">
                                 <div class="left_post_navasd">
                                     <?php get_template_part('content', 'entry');?>
                                 </div>
