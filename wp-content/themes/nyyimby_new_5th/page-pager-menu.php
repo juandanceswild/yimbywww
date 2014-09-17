943d208220b563c34695834aa7b1e2fb2eed209e
Jay Johnston <jay@webjuju.com>
2014-09-17 16:58:24 -0700

    fixed infinite scrolling ajax simultaneously optimizing it to send 90% fewer characters during pagination ajax refreshes

--- wp-content/themes/nyyimby_new_5th/page-pager-menu.php
+++ wp-content/themes/nyyimby_new_5th/page-pager-menu.php
@@ -1,6 +1,34 @@
+<?php // start of sidebar_leftTABS.php ?>
+<?php
+//die('<pre>'.print_r($_SERVER['REQUEST_URI'],true));
+    preg_match('!pager-menu\/([0-9]*)!', $_SERVER['REQUEST_URI'], $matches);
+    $paged = @$matches[1];
+    $postsperpage = 22;
+    if (!$paged) $paged = 1;
+    if ( is_single() )
+    {
+        $current_post = $post->ID;
+        $single = true;
+    } else {
+        $single = false;
+    }
+
+
+    $temp_post = $post; // Storing the object temporarily
+    $temp = $wp_query;
+    $wp_query = null;
+    $wp_query = new WP_Query();
+    $wp_query->query('showposts='.$postsperpage.'&post_type=post&post_status=publish'.'&paged='.$paged);
+
+    $first_date = true;
+    $first = true;
+
+?>
+
+<?php // this was the beginning of main_tab.php ?>
 <div class="scroll_wrapper">
     <div class="padding content sidebar_nav_inner_news" id="sidebar_nav_inner">
-        <div class="test_inner_news">
+        <div class="test_inner_news p-sm">
 
             <ul class="media-list left_post_nav">
 
@@ -64,7 +92,7 @@
             <!-- Pagination -->
             <ul>
                 <li class="next">
-                    <a href="<?php echo home_url(); ?>/page/<?php echo $paged+1; ?>" class="next_link">
+                    <a href="<?php echo home_url(); ?>/pager-menu/<?php echo $paged+1; ?>" class="next_link">
                         <span class="meta-nav">&larr;</span> More posts
                     </a>
                 </li>
@@ -75,3 +103,14 @@
         </div> <!-- // div.test_inner_news -->
     </div> <!-- // div.content -->
 </div> <!-- // div.scroll_wrapper -->
+<?php // end of old main_tab.php ?>
+
+<?php
+    global $nav;
+    $nav = $wp_query;
+
+    $wp_query = null;
+    $wp_query = $temp;  // Reset
+    $post = $temp_post; // Restore the value of $post to the original
+?>
+<?php // end of old sidebar_leftTABS.php ?>
