5201e1962ca61dd9df2b235361ca1071f0bc0b7d
Jay Johnston <jay@webjuju.com>
2014-09-30 06:44:48 -0700

    set viewport meta to match bootstrap 3 recommendation

--- wp-content/themes/nyyimby_new_5th/header.php
+++ wp-content/themes/nyyimby_new_5th/header.php
@@ -37,7 +37,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
