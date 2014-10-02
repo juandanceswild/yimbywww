127ac75e1edb5c3916ce4fedd3326707cbc1548a
Jay Johnston <jay@webjuju.com>
2014-10-01 21:51:45 -0700

    fixed html indention

--- wp-content/themes/nyyimby_new_5th/page-pager-page.php
+++ wp-content/themes/nyyimby_new_5th/page-pager-page.php
@@ -2,23 +2,22 @@
 $args = get_args(1);
 ?>
 
-      <div id="post-<?php the_ID(); ?>" <?php post_class('on-page-post'); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class('on-page-post'); ?>>
 
+    <div class="post-wrapper bordered">
+        <h1 class="entry-title"><?php the_title();?></h1>
+        <span class="vcard author post-author">
+            <span class="fn">
+                <h4 class="by_line">By: <?php the_author() ?> on <span class="post-date updated"><?php the_time('F jS Y'); ?> at <?php the_time('g:i a'); ?></span></h4>
+            </span>
+        </span>
 
-        <div class="post-wrapper bordered">
-              <h1 class="entry-title"><?php the_title();?></h1>
-              <span class="vcard author post-author">
-                  <span class="fn">
-                    <h4 class="by_line">By: <?php the_author() ?> on <span class="post-date updated"><?php the_time('F jS Y'); ?> at <?php the_time('g:i a'); ?></span></h4>
-                  </span>
-              </span>
-
-              <div class="main_image_wrapper">
-                <a href="#" class="thumbnail">
+        <div class="main_image_wrapper">
+            <a href="#" class="thumbnail">
                   <?php echo the_post_thumbnail('full', array('class'=>'w75'));  ?>
                   <?php $image_data = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "full" ); ?>
-                </a>
-                <div id="main_image_thumbnails">
+            </a>
+            <div id="main_image_thumbnails">
                   <?php if (class_exists('MultiPostThumbnails')) :?>
                   <?php
                   $second_image_full = MultiPostThumbnails::get_post_thumbnail_url(get_post_type(), 'second-image', NULL);
@@ -47,26 +46,25 @@ $args = get_args(1);
                   'alt' => get_post(MultiPostThumbnails::get_post_thumbnail_id(get_post_type(), 'sixth-image', get_the_ID()))->post_excerpt));?>
                 <?php endif;?>
               </div>
-            </div>
-            <div class="text-center">
+        </div>
+        <div class="text-center">
               <p id="photo_caption"><?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?></p>
-            </div>
-            <p><?php the_content( the_ID()); ?></p>
-
-            <p style="margin-top:14px;">Posted in <?php the_category(' | '); ?>
-                <?php if(is_archive()): ?>
-                    <!-- <div class="separator"></div> -->
-
-                    <div class="archive-meta">
-                        <a href="<?php echo get_permalink( ); ?>" tilte="<? echo the_title(); ?>">
-                            <? echo the_title(); ?>
-                        </a>
-                    </div> <!-- // div.archive-meta -->
-                <?php endif; ?>
-            </p>
-
-            <?php $current_post_id = $post->ID;?>
         </div>
 
-          <div class="clearfix"></div>
+        <p><?php the_content( the_ID()); ?></p>
+
+        <div style="margin-top:14px;">Posted in <?php the_category(' | '); ?>
+            <?php if(is_archive()): ?>
+                <div class="archive-meta">
+                    <a href="<?php echo get_permalink( ); ?>" title="<?php echo the_title(); ?>">
+                        <?php echo the_title(); ?>
+                    </a>
+                </div>
+            <?php endif; ?>
+        </div>
+
+        <?php $current_post_id = $post->ID;?>
+    </div>
+
+    <div class="clearfix"></div>
 </div>
